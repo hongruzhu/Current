4305074d82ba3feedeb25e41257b2d757c165307
hongruzhu <hongru07@gmail.com>
2023-04-17 20:34:38 +0800

    [refactor] Update peerJS host and port

--- public/src/stream.js
+++ public/src/stream.js
@@ -1,7 +1,7 @@
 let socket = io();
 const myPeer = new Peer(undefined, {
-  host: "localhost", // currentmeet.com
-  port: "3001", // 443
+  host: "currentmeet.com", // currentmeet.com
+  port: "443", // 443
   path: "/myapp",
   debug: 2,
 });
