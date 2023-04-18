b35421a15f6e130d720cb02e90767a92b076045a
hongruzhu <hongru07@gmail.com>
2023-04-18 21:42:15 +0800

    [feature] Add function about recording user numbers in the room by Redis. If no user in the room, delete roomId from Redis and release the roomId for other users

--- server/controllers/socketio_controller.js
+++ server/controllers/socketio_controller.js
@@ -1,3 +1,12 @@
 import { redis } from "../util/cache.js";
 
-const recordRoomUsers = (roomId, peerId) => {};
+const reviseRoomUserNumber = (roomId, number) => {
+  const count = redis.hincrby("room", roomId, number);
+  return count;
+};
+
+const deleteRoomId = (roomId) => {
+  redis.hdel("room", roomId);
+}
+
+export { reviseRoomUserNumber, deleteRoomId };
--- server/routes/socketio_route.js
+++ server/routes/socketio_route.js
@@ -1,11 +1,21 @@
+import {
+  reviseRoomUserNumber,
+  deleteRoomId,
+} from "../controllers/socketio_controller.js";
+
 const liveStreaming = (io, socket) => {
   socket.on("join-room", (roomId, peerId) => {
     socket.join(roomId);
     socket.to(roomId).emit("user-connected", peerId);
-    
-    socket.on("disconnect", () => {
-    socket.to(roomId).emit("user-disconnected", peerId);
-  });
+    reviseRoomUserNumber(roomId, 1);
+
+    socket.on("disconnect", async () => {
+      socket.to(roomId).emit("user-disconnected", peerId);
+      const count = await reviseRoomUserNumber(roomId, -1);
+      if (count === 0) {
+        deleteRoomId(roomId);
+      }
+    });
   });
 }
 
