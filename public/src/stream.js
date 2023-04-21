968d6f23a985ffe182fc181b3fadebc9513b19bd
hongruzhu <hongru07@gmail.com>
2023-04-21 21:50:37 +0800

    chore: update PeerJS port

--- public/src/stream.js
+++ public/src/stream.js
@@ -69,7 +69,7 @@ const myStream = await convertCanvasToStream(canvasElement);
 let socket = io();
 const myPeer = new Peer(undefined, {
   host: "currentmeet.com", // currentmeet.com
-  port: "403", // 443
+  port: "443", // 443
   path: "/myapp",
   debug: 2,
 });
