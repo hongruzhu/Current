69770b51eac2f3d3161fe7ab3436de7337dab1db
hongruzhu <hongru07@gmail.com>
2023-05-05 19:03:10 +0800

    [style] Redesign index page

--- public/src/index.js
+++ public/src/index.js
@@ -83,10 +83,10 @@ $("#signon").on("click", () => {
 let swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   centeredSlides: true,
-  // autoplay: {
-  //   delay: 3500,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay: 3500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
