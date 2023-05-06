10aa26ad3bb741e6a825fa08ce237066ea421429
hongruzhu <hongru07@gmail.com>
2023-05-06 12:04:05 +0800

    [style] Redesign login status on index page

--- public/src/index.js
+++ public/src/index.js
@@ -23,12 +23,36 @@ try {
 
 
 if (logInStatus) {
+  const userName = localStorage.getItem("userName");
+  const userEmail = localStorage.getItem("userEmail");
   $("#navbar").append(`
-    <div class="px-2 ml-auto">
-      <a id="signout" type="button" href="./"
-        class="text-orange-700 hover:text-white border border-orange-700 hover:bg-orange-800 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-orange-500 dark:focus:ring-orange-800">登出</a>
+    <div id="user-profile" class="flex justify-center items-center gap-4 px-2 ml-auto">
+      <div class="font-medium text-balck text-right">
+        <div class="text-base text-black">${userName}</div>
+        <div class="text-base text-gray-500">歡迎使用 Current</div>
+      </div>
+      <button class="avatar" type="button" data-dropdown-toggle="userDropdown" data-dropdown-placement="bottom-start">
+        <div class="w-[45px] h-[45px] rounded-full ring ring-primary ring-yellow-600 ring-offset-base-100 ring-offset-2">
+          <img src="../images/user.png" />
+        </div>
+      </button>
     </div>
-    <a class="bg-[url('../images/user.png')] w-[50px] h-[50px] bg-contain" href=""></a>
+    <!-- Dropdown menu -->
+    <div id="userDropdown"
+      class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-lg w-52">
+      <div class="px-4 py-3 text-base text-gray-900">
+        <div>您的 Email</div>
+        <div class="font-medium truncate">${userEmail}</div>
+      </div>
+      <ul class="py-2 text-base text-gray-700" aria-labelledby="avatarButton">
+        <li>
+          <a href="/" class="block px-4 py-2 hover:bg-gray-100">個人資訊</a>
+        </li>
+        <li>
+          <a id="signout" href="/" class="block px-4 py-2 hover:bg-gray-100">登出</a>
+        </li>
+      </ul>
+    </div>
   `);
   $("#signout").click(() => {
     localStorage.removeItem("accessToken");
