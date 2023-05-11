81682f8b8db2cbf1582886e49f30c84ee93f9b36
hongruzhu <hongru07@gmail.com>
2023-05-12 01:36:50 +0800

    fix: fix string recognition bug in begin.js file
    - Fix a bug where `null` was not being recognized as a string in `begin.js`
    
    Signed-off-by: hongruzhu <hongru07@gmail.com>

--- public/src/begin.js
+++ public/src/begin.js
@@ -8,7 +8,7 @@ const myPicture = localStorage.getItem(`userPicture`);
 const socket = io();
 
 // 若有上傳大頭貼，把自己的大頭貼append到視訊頁面上
-if (myPicture === null) {
+if (myPicture === "null") {
   $("#myVideo img").attr("src", "/images/user-hide-camera.png");
 } else {
   $("#myVideo img").attr("src", `/uploads/${myPicture}`);
