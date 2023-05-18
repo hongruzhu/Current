08c3f9d86bdfc0d24950bd9a05cda5ec7f64c130
hongruzhu <hongru07@gmail.com>
2023-05-19 01:15:41 +0800

    [refactor] Revise frontend code to fix AWS CloudFront bugs

--- server/util/cache.js
+++ server/util/cache.js
@@ -4,23 +4,44 @@ dotenv.config();
 
 const { CACHE_HOST, CACHE_PORT, CACHE_USER, CACHE_PASSWORD } = process.env;
 
-const redis = new Redis({
-  host: CACHE_HOST,
-  port: CACHE_PORT,
-  retryStrategy: function (times) {
-    // 檢查最大重試次數
-    if (times >= 10) {
-      // 返回一個錯誤，觸發MaxRetriesPerRequestError
-      return new Error(
-        "Redis returned too many errors, MaxRetriesPerRequestError"
-      );
-    }
-    // 等待時間遞增，最多等2000ms
-    return Math.min(times * 50, 2000);
-  },
-  username: CACHE_USER,
-  password: CACHE_PASSWORD,
-  tls: {}
-});
+let redis;
+if (process.env.NODE_ENV === "production") {
+  redis = new Redis({
+    host: CACHE_HOST,
+    port: CACHE_PORT,
+    retryStrategy: function (times) {
+      // 檢查最大重試次數
+      if (times >= 10) {
+        // 返回一個錯誤，觸發MaxRetriesPerRequestError
+        return new Error(
+          "Redis returned too many errors, MaxRetriesPerRequestError"
+        );
+      }
+      // 等待時間遞增，最多等2000ms
+      return Math.min(times * 50, 2000);
+    },
+    username: CACHE_USER,
+    password: CACHE_PASSWORD,
+    tls: {}
+  });
+} else {
+  redis = new Redis({
+    host: CACHE_HOST,
+    port: CACHE_PORT,
+    retryStrategy: function (times) {
+      // 檢查最大重試次數
+      if (times >= 10) {
+        // 返回一個錯誤，觸發MaxRetriesPerRequestError
+        return new Error(
+          "Redis returned too many errors, MaxRetriesPerRequestError"
+        );
+      }
+      // 等待時間遞增，最多等2000ms
+      return Math.min(times * 50, 2000);
+    },
+    username: CACHE_USER,
+    password: CACHE_PASSWORD
+  });
+}
 
 export { redis };
--- tailwind.config.js
+++ tailwind.config.js
@@ -28,4 +28,3 @@ export default {
     darkTheme: "dark",
   },
 };
-
